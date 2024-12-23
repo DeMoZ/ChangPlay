72ffa5699f83c584577d2700d40e34a77ec2598b
Roman Kolychev <demozbox@gmail.com>
2024-12-23 13:58:14 +0700

    Build update

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "roman.kolychev-Chang-0.0.47";
+const cacheName = "roman.kolychev-Chang-0.0.48";
 const contentToCache = [
     "Build/Chang.loader.js",
     "Build/Chang.framework.js",

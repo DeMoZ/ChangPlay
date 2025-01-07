65e0e64b84d83b04bb58db8f376d45dfdee64ded
Roman Kolychev <demozbox@gmail.com>
2025-01-08 03:30:51 +0700

    Build update 50

--- Build/Chang.framework.js
+++ Build/Chang.framework.js
@@ -1284,10 +1284,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  8307312: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 8307367: ($0) => { performance.now = function() { return $0; }; },  
- 8307415: ($0) => { performance.now = function() { return $0; }; },  
- 8307463: () => { performance.now = Module['emscripten_get_now_backup']; }
+  8308032: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 8308087: ($0) => { performance.now = function() { return $0; }; },  
+ 8308135: ($0) => { performance.now = function() { return $0; }; },  
+ 8308183: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
